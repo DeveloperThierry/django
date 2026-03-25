24cfbbe36ed8f5c15cb475e264dd2ccb2e84988c
developerthierry <developerthierry@gmail.com>
2026-03-25 04:40:54 +0000

    Implement Page Details Image

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.homepage),
     path("about/", views.about),
     path("posts/", include("posts.urls"))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
