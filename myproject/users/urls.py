1aae87f89430abcb938f492fade914de32ad9c60
developerthierry <developerthierry@gmail.com>
2026-03-25 06:14:12 +0000

    Implement Auth: Register & Login

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -4,6 +4,7 @@ app_name = "users"
 urlpatterns = [
     path("", views.usershome, name="usershome"),
     path("users_list/", views.users_list, name="users_list"),
-    path("register/", views.register_view, name="register_user")
+    path("register/", views.register_view, name="register_user"),
+    path("login/", views.login_view, name="login_user")
     
 ]
--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 def users_list(request):
     users = User.objects.all()
@@ -14,8 +15,19 @@ def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:posts_list")
     else:
         form = UserCreationForm()
     return render(request, "users/register.html", {"form":form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #LOGIN HERE
+            login(request, form.get_user())
+            return redirect("posts:posts_list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form":form})
